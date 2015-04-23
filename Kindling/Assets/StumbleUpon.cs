892ab73560144dc7d7d141f621c1ef494b8fac13
Dennis Paramonov <dennisparamonov8@gmail.com>
2015-04-23 18:39:20 -0400

    added sounds + model mockups

---
+++ Kindling/Assets/StumbleUpon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StumbleUpon : MonoBehaviour {
+
+	public AudioClip Stumble;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter () {
+
+	
+	}
+}
