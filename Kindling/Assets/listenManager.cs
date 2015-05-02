16df958f7ecd9275f3ad8053e36fa385b64a988b
Andrew Benson <andros818@gmail.com>
2015-05-02 16:26:43 -0400

    created listen/effect script; added light change/focus effect

---
+++ Kindling/Assets/listenManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class listenManager : MonoBehaviour {
+
+	public bool inRange = false;
+	public bool audioPlaying = false;
+	float listenVol;
+	AudioSource audio;
+	public AudioClip listeningAudio;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		
+		if (inRange) {
+			if (audioPlaying == false){
+				if (Input.GetKeyDown (KeyCode.Mouse0)){
+					audio = GetComponent<AudioSource>();
+					audio.PlayOneShot(listeningAudio, 1);
+					audioPlaying = true;
+					Debug.Log ("play audio");
+				}
+			} else {
+				if (Input.GetKey (KeyCode.Mouse0)) {
+					audio.volume = 1;
+				} else if (Input.GetKeyUp(KeyCode.Mouse0)) {
+					audio.volume = 0;
+				}
+			}
+		}
+	
+	}
+
+	void OnTriggerEnter (Collider other){
+		//listenSource = other.gameObject;
+		if (other.gameObject.CompareTag ("Player")) {
+			inRange = true;
+			//Debug.Log ("Near Church");
+		}
+	}
+
+}
