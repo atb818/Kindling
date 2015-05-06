cfb0c7a4661f9b55777fd4406380ba0a63450566
Dennis Paramonov <dennisparamonov8@gmail.com>
2015-05-05 22:15:30 -0400

    added cabin flavor

---
+++ Kindling/Assets/flickerscript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flickerscript : MonoBehaviour {
+
+	float amplitudeX = 0.05f;
+	float amplitudeY = 0.2f;
+	float omegaX = 0.05f;
+	float omegaY = 0.2f;
+	float index;
+	
+	public void Update(){
+		index += Time.deltaTime;
+		float x = amplitudeX*Mathf.Cos (omegaX*index);
+		float y = Mathf.Abs (amplitudeY*Mathf.Sin (omegaY*index));
+		transform.localPosition = new Vector3(x+321,y+45,144);
+	}
+}
