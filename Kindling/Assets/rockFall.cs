0f223ba95f239ebe67661966ab2779565c11409c
Andrew Benson <andros818@gmail.com>
2015-05-07 18:01:09 -0400

    added rockwall at start/dig functional; fixed/placed torch lanterns in town

---
+++ Kindling/Assets/rockFall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rockFall : MonoBehaviour {
+
+	bool falling = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (gameObject.GetComponent<Rigidbody> ().isKinematic == false && transform.localScale.x > .1f) {
+			transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
+		}
+		if (transform.localScale.x <= .1f) {
+			gameObject.SetActive (false);
+		}
+
+	}
+}
