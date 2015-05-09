1be28730aab7d9608ba4051268e73aa366c5a4fd
Andrew Benson <andros818@gmail.com>
2015-05-09 15:39:45 -0400

    dog panting; torches; v1 dead wolf

---
+++ Kindling/Assets/panting.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class panting : MonoBehaviour {
+
+	public AudioClip dogPant;
+	AudioSource audio;
+
+	void Start () {
+
+		audio = GetComponent<AudioSource> ();
+		audio.clip = dogPant;
+	
+	}
+	
+	void Update () {
+
+		if (Input.GetKeyUp (KeyCode.W) || Input.GetKeyUp (KeyCode.UpArrow)) {
+			audio.Play();
+
+		}
+	
+	}
+}
