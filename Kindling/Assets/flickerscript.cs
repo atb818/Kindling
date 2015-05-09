06e87e40b9b187682acb5d800549cc408f058cff
Dennis Paramonov <dennisparamonov8@gmail.com>
2015-05-09 15:47:47 -0400

    cabin prefab

--- Kindling/Assets/flickerscript.cs
+++ Kindling/Assets/flickerscript.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class flickerscript : MonoBehaviour {
 
-	float amplitudeX = 0.05f;
+	float amplitudeX = 0.1f;
 	float amplitudeY = 0.2f;
-	float omegaX = 0.05f;
+	float omegaX = 0.1f;
 	float omegaY = 0.2f;
 	float index;
 	
@@ -13,6 +13,6 @@ public class flickerscript : MonoBehaviour {
 		index += Time.deltaTime;
 		float x = amplitudeX*Mathf.Cos (omegaX*index);
 		float y = Mathf.Abs (amplitudeY*Mathf.Sin (omegaY*index));
-		transform.localPosition = new Vector3(x+321,y+45,144);
+		transform.localPosition = new Vector3(x+321,y+44,144);
 	}
 }
