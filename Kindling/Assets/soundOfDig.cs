5f906a1314e61c1ca22d38c34fcad8a99fb13989
Andrew Benson <andros818@gmail.com>
2015-05-13 16:23:40 -0400

    lantern stuff; audio - rockfall, digging; collider over rockwall; decreased angle of dig; shaders on cow/bluhd

---
+++ Kindling/Assets/soundOfDig.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soundOfDig : MonoBehaviour {
+
+	AudioSource audio;
+	public AudioClip diggin;
+
+	void Start () {
+	
+		audio = gameObject.GetComponent<AudioSource> ();
+
+	}
+	
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.Mouse1)) {
+			audio.Play (); 
+		} else if (Input.GetKeyUp (KeyCode.Mouse1)) {
+			audio.Stop ();
+		}
+
+	}
+}
