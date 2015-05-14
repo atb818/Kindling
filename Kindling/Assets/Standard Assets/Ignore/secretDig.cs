f5a72f566adaa16f5599d44207ed20f390154ee2
Andrew Benson <andros818@gmail.com>
2015-05-14 13:50:28 -0400

    HINGES (& stuff)

---
+++ Kindling/Assets/Standard Assets/Ignore/secretDig.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class secretDig : MonoBehaviour {
+
+	bool secretFound = false;
+
+	GameObject [] hinges;
+
+
+	void Start () {
+		secretFound = false;
+
+		hinges = GameObject.FindGameObjectsWithTag ("oriensSecret");
+		/*foreach(GameObject hinge in hinges){
+			hinge.SetActive (false);
+		}*/
+
+	}
+	
+	void OnTriggerStay (Collider other){
+		if (Input.GetKeyUp (KeyCode.Mouse1) && secretFound == false && other.gameObject.CompareTag ("Player")) {
+			foreach(GameObject hinge in hinges){
+				hinge.SetActive (true);
+			}
+			secretFound = true;
+		}
+	}
+}
